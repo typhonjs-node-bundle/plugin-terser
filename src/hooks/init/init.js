cfbd33f0c2dac642d46195f8a3255a0fd7af644d
typhonrt <support@typhonrt.org>
2020-12-21 00:28:10 -0800

    added dependencies / flags

--- src/hooks/init/init.js
+++ src/hooks/init/init.js
@@ -1,3 +1,5 @@
+const { flags }   = require('@oclif/command');
+
 /**
  * Handles interfacing with the plugin manager adding event bindings to pass back a configured
  * instance of `rollup-plugin-terser`.
@@ -49,6 +51,36 @@ module.exports = async function(opts)
 {
    process.pluginManager.add({ name: 'plugin-terser', instance: PluginHandler });
 
+   // Adds flags for various built in commands like `build`.
+   s_ADD_FLAGS(opts.id);
+
    // TODO REMOVE
    process.stdout.write(`plugin-terser init hook running ${opts.id}\n`);
 };
+
+/**
+ * Adds flags for various built in commands like `build`.
+ *
+ * @param {string} commandID - ID of the command being run.
+ */
+function s_ADD_FLAGS(commandID)
+{
+   switch (commandID)
+   {
+      // Add all built in flags for the build command.
+      case 'build':
+         process.eventbus.trigger('oclif:flaghandler:add', {
+            // By default compress is set to true, but if the environment variable `DEPLOY_COMPRESS` is defined as
+            // 'true' or 'false' that will determine the setting for compress.
+            compress: flags.boolean({
+               'description': 'Compress output using Terser.', 'default': function()
+               {
+                  if (process.env.DEPLOY_COMPRESS === 'true') { return true; }
+
+                  return process.env.DEPLOY_COMPRESS !== 'false';
+               }
+            }),
+         });
+         break;
+   }
+}
